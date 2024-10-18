02eda8603fbbe2a0f45c8498b49e0e9e0dfe0b7e
DiegoMuniz92 <diegooliveiramuniz2014@gmail.com>
2024-10-18 12:50:51 -0300

    break e continue

--- aula17(break_continue).js
+++ aula17(break_continue).js
@@ -2,16 +2,24 @@ let n = 0;
 let max = 20;
 let pares = 0;
 
-while(n<max) {
+/*while(n<max) {
     console.log("CFB cursos . " + n)
     if(n>10){
         break
     }
-    if(n%2!=0){
-        continue
-        pares++
-    }
     n++
+}*/
+
+for(let i = n; i < max; i++){
+    if(i%2!=0){
+        continue
+        
+    }
+    pares++
 }
 console.log("quantidade de numeros pares: " + pares);
 console.log("Fim do programa");
+
+/*
+
+*/
